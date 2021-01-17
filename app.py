72816010752117ac3a14aa7fec4c2a206b2a8dba
titanium <titanium@nectar.ark>
2021-01-17 18:16:35 +0800

    Validation for user input on continue program

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
 print("\n")
 print("=================================================================")
-print("========== WELCOME TO THE 🍏 HEXIDECIMAL CALCULATOR ==========")
+print("========== WELCOME TO THE 🍐 HEXIDECIMAL CALCULATOR ==========")
 print("=================================================================")
 
-ans = 'y'
+selection = ['y', 'n']
+
+# selection = ['y', 'n', 'menu', 'exit']
+#     for m in selection[2::]:
+#         print(m)
+#         if m == n.lower():
+#             break
+user_sel = 'y'
 
 def calculator(n):
+   
     h = str(hex(int(n)))
     z = '00000'
     apple = ''
@@ -28,22 +36,32 @@ def calculator(n):
 
     return apple
 
-while ans == 'y':
+while user_sel == 'y':
     print("\n")
-    n = input("Enter the number or MB to convert: ")
     try:
+        n = input("Enter the number or MB to convert: ")
         calculator(n)
     except ValueError:
         print("*****YOU MAY ONLY ENTER A NUMBER. Please try again*****")
+        # print("To exit the calculator, please type 'exit'. ")
         continue
+    
+    user_sel = input("Would you like to calculate another number? Please type 'y' or 'n': ")
+    user_sel_bad = True
 
-
-    ans = input("Would you like to calculate another number? Please answer 'y' or 'n': ")
+    while user_sel_bad:
+        try:
+            next(user_sel for s in selection if user_sel == s)
+            user_sel_bad = False
+        except StopIteration:
+            user_sel = input("Please ONLY type 'y' or 'n': ")
         
-    if ans == 'n':
+    if user_sel == 'n':
         print("\n")
         print("=============================================================")
         print("======================= GOOD BYE ============================")
         print("=============================================================")
         print("\n")
         break
+
+#help(pyinputplus.parameters)
